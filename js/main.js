71d18e77013ba14aa9bdf40d0bd93f48673067cd
jumgal <akaev.jumgal@gmail.com>
2018-03-25 16:31:49 +0600

    Improved validation of forms. New CDN links are added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,75 @@
 
 $('document').ready( function() {
+    /*For mobile version of left side bar*/
     $(".button-collapse").sideNav();
-    /*jquery plugin for work with tables*/
-});
 
+/*Registration form validation*/
+    $("#reg_form").validate({
+        rules: {
+            first_name: {
+                required: true
+            },
+            last_name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            pass: {
+                required: true,
+                minlength: 6
+            },
+            confirm_password: {
+                required: true,
+                equalTo: "#password"
+            }
+        },
+        messages: {
+            first_name:{
+                required: "Заполните поле"
+            },
+            last_name:{
+                required: "Заполните поле"
+            },
+            email: {
+                required: "Заполните поле"
+            },
+            pass: {
+                required: "Заполните поле",
+                minlength: "Не менее 6 символов"
+            },
+            confirm_password: {
+                required: "Заполните поле",
+                equalTo: "Пароли должны совпадать"
+            }
+        },
+        errorElement: "span"
+    });
+    /*Authorization form validation*/
+    $('#authForm').validate({
+        rules: {
+            login: {
+                required: true
+            },
+            authPassword: {
+                required: true,
+                minlength: 6
+            }
+        },
+        messages: {
+            login: {
+                required: "Заполните поле"
+            },
+            authPassword: {
+                required: "Заполните поле",
+                minlength: "Не менее 6 символов"
+            }
+        },
+        errorElement: "span"
+    })
+});
+/*Rendering tables with lesson*/
 function renderTable() {
 
     var table = $('<table>', { "id": "group", "class": "mdl-data-table"}).appendTo('#content');
@@ -14,6 +80,7 @@ function renderTable() {
     var grade = $('<th>').html("Оценка").appendTo(tr);
     var edit = $('<th>').html("Изменить").appendTo(tr);
 
+/*jquery plugin for work with tables*/
     $('#group').DataTable({
         "ajax": '../arrays.json',
         "language": {
@@ -30,11 +97,10 @@ $('#showPassword').click(function(){
 }
 });
 
-function addNewGroup() {
-    var li = $('<li>', { "id": "inGroup" }).insertBefore("#newGroup");
-    var input = $('<input>', {"type": "text", "placeholder": "введите название"}).appendTo(li);
-
-    var newLi = $('<li>').insertBefore("#inGroup");
-    var a = $('<a>', { "href": "#", "class": "waves-effect"}).html(input.val()).appendTo(newLi);
-
-};
+// function addNewGroup() {
+//     var li = $('<li>', { "id": "inGroup" }).insertBefore("#newGroup");
+//     var input = $('<input>', {"type": "text", "placeholder": "введите название"}).appendTo(li);
+//
+//     var newLi = $('<li>').insertBefore("#inGroup");
+//     var a = $('<a>', { "href": "#", "class": "waves-effect"}).html(input.val()).appendTo(newLi);
+// };
